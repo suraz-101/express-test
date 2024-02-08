414015af32e33329a93a795b7da7a83f908e7819
Suraj Pandey <surazpandey101@gmail.com>
2024-02-08 09:56:51 +0545

    learnig foreign key connection

--- modules/blogs/blog.model.js
+++ modules/blogs/blog.model.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
 
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: [true, "Tile is missing"] },
   tags: { type: String }, //["Science", "mern-stack"] ??
   content: { type: String },
-  author: { type: String, required: [true, "Author is missing"] },
+  author: {
+    type: ObjectId,
+    ref: "User",
+    required: [true, "Author is missing"],
+  },
   words: { type: Number, default: 0 },
   status: { type: String, enum: ["published", "draft"], default: "draft" }, // [ published, draft] ??
   createAt: { type: Date, default: Date.now },
