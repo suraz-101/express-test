f1d3947c77e5105998917217290729fec9048823
Suraj Pandey <surazpandey101@gmail.com>
2024-02-08 17:55:01 +0545

    learning validation using joi in mongoose database

--- modules/users/user.validate.js
+++ modules/users/user.validate.js
@@ -12,11 +12,7 @@ const Schema = Joi.object({
 const validate = (req, res, next) => {
   const { error } = Schema.validate(req.body);
 
-  if (error) {
-    res.status(400).json({ message: error.details[0].message });
-  } else {
-    next();
-  }
+  error ? res.status(400).json({ message: error.details[0].message }) : next();
 };
 
 module.exports = { validate };
