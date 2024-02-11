ba5bef77090df22de5a544bc0363ca5e1587cca4
Suraj Pandey <surazpandey101@gmail.com>
2024-02-11 23:28:02 +0545

    including .env file in gitignore

--- utils/sessionManager.js
+++ utils/sessionManager.js
@@ -4,6 +4,7 @@ const checkRole = (sysRole) => {
   return (req, res, next) => {
     const { access_token } = req.headers || "";
     if (!access_token) throw new Error("Access Token is required");
+    // const tokenVerification = verif
     const { data } = verifyJWT(access_token);
     const isValidRole = sysRole.some((role) => data.role.includes(role));
     if (!isValidRole) throw new Error("Permission Denied");
