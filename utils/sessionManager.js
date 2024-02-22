bdc6c6a8e28e20e9477d0acd3a69335d78beb20e
Suraj Pandey <surazpandey101@gmail.com>
2024-02-22 08:31:25 +0545

    simple modification

--- utils/sessionManager.js
+++ utils/sessionManager.js
@@ -3,18 +3,23 @@ const userModel = require("../modules/users/user.model");
 
 const checkRole = (sysRole) => {
   return async (req, res, next) => {
-    const { access_token } = req.headers || "";
-    if (!access_token) throw new Error("Access Token is required");
-    const { data } = verifyJWT(access_token);
-    const isValidRole = sysRole.some((role) => data.role.includes(role));
-    if (!isValidRole) throw new Error("Permission Denied");
-    const { role, email } = data;
-    const user = await userModel.findOne({ email });
-    req.body.author = role.includes("user")
-      ? user._id.toString()
-      : req.body.author;
+    try {
+      const { access_token } = req.headers || "";
+      if (!access_token) throw new Error("Access Token is required");
+      const { data } = verifyJWT(access_token);
 
-    next();
+      const isValidRole = sysRole.some((role) => data.role.includes(role));
+      if (!isValidRole) throw new Error("Permission Denied");
+      const { role, email } = data;
+      const user = await userModel.findOne({ email });
+      req.body.author = role.includes("user")
+        ? user._id.toString()
+        : req.body.author;
+
+      next();
+    } catch (error) {
+      next(error);
+    }
   };
 };
 
